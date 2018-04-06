b37d0d0a5d7d518097088a3cf923bb6262150f91
Stephen Clark <contact.stephenclark@gmail.com>
2018-04-06 17:49:15 +0100

    Added naiveFib.py

---
+++ naiveFib.py
@@ -0,0 +1,8 @@
+"""return the nth term of a fib sequence using a naive method"""
+def fibonacci (n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
